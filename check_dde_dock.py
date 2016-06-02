acbbc289ff85f5ae6b9e2f891a5ba43b4ff252bb
wangyingtaodeepin <wangyingtao@linuxdeepin.com>
2016-06-02 17:56:08 +0800

    Add check dde-dock taskgit add -A!

---
+++ check_dde_dock.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import psutil
+import sys
+import time
+
+def isTaskExist(name):
+    for pid in psutil.pids():
+        p = psutil.Process(pid)
+        if p.name() == name:
+            print("Task %s exist." % name)
+            return True
+    return False
+
+timeout = 180
+
+while True:
+    if isTaskExist("dde-dock"):
+        time.sleep(30)
+        sys.exit(0)
+    elif timeout != 0:
+        continue
+    else:
+        sys.exit(1)
+
